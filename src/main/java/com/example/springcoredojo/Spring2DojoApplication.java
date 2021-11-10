1be381dd02d01c7e6dad95c7050d917dc6678e33
Jose Eduardo Garcia Luna <jgarluna@everis.com>
2021-11-09 18:11:07 -0600

    Uso Properties

---
+++ src/main/java/com/example/springcoredojo/Spring2DojoApplication.java
@@ -0,0 +1,25 @@
+package com.example.springcoredojo;
+
+
+import com.example.springcoredojo.entidades.Jugador;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Spring2DojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring2DojoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+		return args -> {
+			Jugador jugador= ctx.getBean(Jugador.class);
+			System.out.println(jugador);
+		};
+	}
+}
